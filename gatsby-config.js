9af8d63ed5c7bd49b55b203df094672ebdb36694
Martin Gras <martin.gras95@gmail.com>
2020-02-13 08:59:21 +0000

    commit test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Titi & Martin Travel Blog',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This repo contains the blog website for titi & martin',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
